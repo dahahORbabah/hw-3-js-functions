f1c079a1ed7754bda6fa02b4379b9066b400fd76
Darya Lobanova <loisering@gmail.com>
2019-12-20 04:28:42 +0300

    Added myBind function

--- Lobanova Daria/functions/myBind.js
+++ Lobanova Daria/functions/myBind.js
@@ -1,6 +1,8 @@
-function myBind() {
-    console.log('MyBind function');
-    
+module.exports = Function.prototype.myBind = function(context) {
+    var func = this;
+    var bindArgs = [].slice.call(arguments, 1);
+    return function() {
+        var funcArgs = bindArgs.concat([].slice.call(arguments));
+        return func.apply(context, funcArgs);
+    }   
 }
-
-module.exports = myBind
--- Lobanova Daria/index.js
+++ Lobanova Daria/index.js
@@ -21,55 +21,44 @@ console.log('Enter the number of function -> ');
 r.on('line', function(line) {
     switch(line.trim()) {
         case '1':
-            //input string and separator
-            // r.question('Enter str: ', function (str) {
-            //     r.question('Enter sp: ', function (sp) {
-            //         splitAndMerge(str, sp);
-            //         // r.close();
-            //     });
-            // });
             console.log(splitAndMerge('Hello world', ',')); 
             console.log(splitAndMerge('My name is Jonh', ' '));                      
             break;
         case '2':
-            //input in while 2 param`s
             console.log(convert({name: 'Jeremy', age: 24, role: 'Software Engineer'}));            
             break;
         case '3':
-            //input string
             console.log(toCamelCase('The_Stealth_Warrior'));
             console.log(toCamelCase('the-stealth-warrior'));            
             break;
         case '4':
-            //input string
             console.log(reverse('A fun little challenge!'));            
             break;
         case '5':
-            //input string
             console.log(stringExpansion('3D2a5d2f'));
             console.log(stringExpansion('3d332f2a'));
             console.log(stringExpansion('abcde'));
             break;
         case '6':
-            //input number of elements and the array
             console.log('Largest -> ' + largest(2, 0.1, -5, 100, 3));
             console.log('Smallest -> ' + smallest(2, 0.1, -5, 100, 3));
             break;
         case '7':
-            //input: 1.number of elements 2.the array 3.the element to find
             console.log(transform([10, 20, 30, 40, 50])[3]());
             console.log(transform([10, 20, 30, 40, 50])[4]());
             break;
         case '8':
-            //input: 1.number of elements 2.the array
             console.log(sum(1, 3, 5, 7));
             break;
         case '9':
-            //input the integer number
             console.log(countDown(3));
             break;
         case '10':
-            console.log(myBind());
+            function addPropToNumber(number) {
+                return this.prop + number;
+            }
+            var bound = addPropToNumber.myBind({prop: 9});
+            console.log(bound(1));
             break;
         default:
             process.exit(0);
